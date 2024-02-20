e2f95b48ba4e7bba9a02b36b1ad40dae634b27e7
Makbelhailu <mcwelson92@gmail.com>
2024-02-20 20:46:31 +0300

    jobBoard code added

---
+++ JobBoard/client/src/App.jsx
@@ -0,0 +1,73 @@
+import { Outlet, Navigate, Route, Routes, useLocation } from "react-router-dom";
+import { Footer, Navbar } from "./components";
+import {
+  About,
+  AuthPage,
+  Companies,
+  CompanyProfile,
+  FindJobs,
+  JobDetail,
+  UploadJob,
+  UserProfile,
+  Application
+} from "./pages";
+import { useSelector } from "react-redux";
+
+function Layout() {
+  //const user = true;
+  const { user } = useSelector((state) => state.user);
+  const location = useLocation();
+
+  return user?.token ? (
+    <Outlet />
+  ) : (
+    <Navigate to="/user-auth" state={{ from: location }} replace />
+  );
+}
+
+function App() {
+  const { user } = useSelector((state) => state.user);
+  return (
+    <>
+    <main className='flex-1 bg-[#f7fdfd]'>
+    
+      <Navbar />
+
+      <Routes>
+        {/* Pages accessible when authenticated*/}
+        <Route element={<Layout />}>
+          <Route
+            path="/"
+            element={<Navigate to="/find-jobs" replace={true} />}
+          />
+          <Route path="/find-jobs" element={<FindJobs />} />
+          <Route path="/companies" element={<Companies />} />
+          <Route
+            path={
+              user?.accountType === "seeker"
+                ? "/user-profile"
+                : "/user-profile/:id"
+            }
+            element={<UserProfile />}
+          />
+
+          <Route path={"/company-profile"} element={<CompanyProfile />} />
+          <Route path={"/company-profile/:id"} element={<CompanyProfile />} />
+          <Route path={"/applications"} element={<Application />} />
+          <Route path={"/upload-job"} element={<UploadJob />} />
+          <Route path={"/job-detail/:id"} element={<JobDetail />} />
+        </Route>
+
+        <Route path="/about-us" element={<About />} />
+        <Route path="/user-auth" element={<AuthPage />} />
+      </Routes>
+      {/* Pages accessible when NOT authenticated*/}
+
+      {/*Shows footer only when logged in */}
+      {user && <Footer />}
+    </main>
+      </>
+  );
+}
+
+export default App;
